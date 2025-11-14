920532169c86ab7fc3b0dd46fcb95112782c0c54
LyokoJeremie <lucheng989898@gmail.com>
2025-11-14 15:01:42 +0800

    .

--- src/REGISTRY_Table.ts
+++ src/REGISTRY_Table.ts
@@ -1,6 +1,6 @@
 import {MavLinkPacketRegistry} from "node-mavlink";
-import * as minimalACFly from "../Owl02Lib/minimalACFly";
-import * as commonACFly from "../Owl02Lib/commonACFly";
+import * as minimalACFly from "./Owl02Lib/minimalACFly";
+import * as commonACFly from "./Owl02Lib/commonACFly";
 
 // create a registry of mappings between a message id and a data class
 // 注册表，用于将消息ID映射到数据类
